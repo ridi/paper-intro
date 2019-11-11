8c08cd375e37f5feadb368e87844323afad7e0f1
Wonwoo Choi <wonwoo.choi@ridi.com>
2019-11-11 15:04:21 +0900

    Initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+  siteMetadata: {
+    title: 'RIDIPAPER - 리디페이퍼',
+    description: '리디페이퍼, 세상이 나의 서재가 된다',
+    author: 'RIDI',
+  },
+  plugins: [
+    'gatsby-plugin-typescript',
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'images',
+        path: path.resolve(__dirname, 'src/images'),
+      },
+    },
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
+  ],
+};
