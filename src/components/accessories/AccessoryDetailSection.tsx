399a5cf1da5ba723b55f71c55387cf781d9e1cef
Wonwoo Choi <wonwoo.choi@ridi.com>
2019-11-27 15:42:06 +0900

    Add accessory detail section component

--- src/components/accessories/AccessoryDetailSection.tsx
+++ src/components/accessories/AccessoryDetailSection.tsx
@@ -2,7 +2,9 @@ import styled from 'astroturf';
 import React from 'react';
 
 import { FluidObject } from 'gatsby-image';
-import PolyfillImg from 'gatsby-image/withIEPolyfill';
+import Img from 'gatsby-image';
+
+import LineBreakText from '../LineBreakText';
 
 export interface DetailSection {
   type: 'detail';
@@ -16,10 +18,73 @@ export interface DetailSection {
   };
 }
 
+const Container = styled.div`
+  word-break: keep-all;
+
+  p {
+    width: 85%;
+    margin-top: 30px;
+
+    @media (max-width: 600px) {
+      width: 100%;
+      margin-top: 20px;
+    }
+
+    &:first-child {
+      margin-top: 0;
+      font-size: 16px;
+      line-height: 1.5em;
+      font-weight: bold;
+      letter-spacing: 2px;
+      text-transform: uppercase;
+      color: rgba(0, 0, 0, 0.5);
+
+      @media (max-width: 600px) {
+        font-size: 14px;
+      }
+    }
+  }
+
+  h2 {
+    margin-top: 20px;
+    text-align: left;
+  }
+`;
+
+const Padded = styled.div`
+  padding: 0 40px;
+
+  @media (max-width: 600px) {
+    padding: 0 20px;
+  }
+`;
+
+const ImgWrapper = styled.div`
+  width: 100%;
+  max-width: 1000px;
+  margin: 50px auto 0;
+
+  @media (max-width: 600px) {
+    margin-top: 40px;
+  }
+`;
+
 interface Props {
   data: DetailSection;
 }
 
 export default function AccessoryDetailSection(props: Props) {
-  return null;
+  const description = props.data.description.replace(/ /g, '\xa0');
+  return (
+    <Container>
+      <Padded>
+        <p>{props.data.name}</p>
+        <h2>{props.data.title}</h2>
+        {Boolean(description) && <p>{description}</p>}
+      </Padded>
+      <ImgWrapper>
+        <Img fluid={props.data.image.childImageSharp.fluid} backgroundColor="#f0f5fa" />
+      </ImgWrapper>
+    </Container>
+  );
 }
--- src/components/accessories/AccessorySection.tsx
+++ src/components/accessories/AccessorySection.tsx
@@ -11,12 +11,34 @@ interface SpecSection {
   }[];
 }
 
-export type Section = DetailSection | SpecSection;
+interface TableSection {
+  type: 'table';
+  rows: {
+    head: string;
+    items: string[];
+  }[];
+}
+
+export type Section = DetailSection | SpecSection | TableSection;
 
 const Section = styled.section`
   display: block;
-  max-width: 1000px;
+  max-width: 1080px;
   margin: 100px auto 0;
+
+  @media (max-width: 600px) {
+    max-width: 1040px;
+    margin-top: 80px;
+  }
+
+  h2 {
+    font-size: 36px;
+    line-height: 1.5em;
+
+    @media (max-width: 600px) {
+      font-size: 30px;
+    }
+  }
 `;
 
 interface Props {
@@ -30,6 +52,8 @@ export default function AccessorySection(props: Props) {
       inner = <AccessoryDetailSection data={props.data} />;
       break;
     case 'spec':
+      break;
+    case 'table':
       break;
     default:
       throw ((_: never) => new Error(`Unknown type: ${_}`))(props.data);
