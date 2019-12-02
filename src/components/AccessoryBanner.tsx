f337a993dcb96b4ee1bbad7da817e6f4e02ed3b8
Wonwoo Choi <wonwoo.choi@ridi.com>
2019-12-02 16:45:48 +0900

    Add accessory banner

--- src/components/AccessoryBanner.tsx
+++ src/components/AccessoryBanner.tsx
@@ -1,6 +1,9 @@
 import styled from 'astroturf';
 import React from 'react';
 
+import { graphql, useStaticQuery } from 'gatsby';
+import { FixedObject } from 'gatsby-image';
+
 import { LinkButton } from './Button';
 
 const Container = styled.section`
@@ -9,7 +12,7 @@ const Container = styled.section`
   margin: 0 auto;
   background-color: #f0f5fa;
 
-  @media (max-width: 600px) {
+  @media (max-width: 800px) {
     text-align: center;
   }
 `;
@@ -20,8 +23,9 @@ const Background = styled.div`
   top: 0;
   width: 100%;
   height: 100%;
+  overflow: hidden;
 
-  @media (max-width: 600px) {
+  @media (max-width: 800px) {
     &::after {
       display: block;
       content: '';
@@ -34,6 +38,18 @@ const Background = styled.div`
       background-color: rgba(0, 0, 0, 0.4);
     }
   }
+
+  > picture {
+    position: absolute;
+    top: 0;
+    right: calc(50% - 600px);
+    width: 1200px;
+
+    @media (max-width: 800px) {
+      right: calc(50% - 400px);
+      width: 800px;
+    }
+  }
 `;
 
 const Content = styled.div`
@@ -41,7 +57,7 @@ const Content = styled.div`
   width: 400px;
   padding: 120px 0 110px 100px;
 
-  @media (max-width: 600px) {
+  @media (max-width: 800px) {
     width: 300px;
     margin: 0 auto;
     padding: 120px 0;
@@ -51,7 +67,7 @@ const Content = styled.div`
     margin-top: 30px;
     font-size: 20px;
 
-    @media (max-width: 600px) {
+    @media (max-width: 800px) {
       font-size: 18px;
       color: white;
     }
@@ -63,7 +79,7 @@ const ContentTitle = styled<'h2', { small?: boolean }>('h2')`
   line-height: 50px;
   text-align: left;
 
-  @media (max-width: 600px) {
+  @media (max-width: 800px) {
     display: none;
     text-align: center;
     color: white;
@@ -74,7 +90,7 @@ const ContentTitle = styled<'h2', { small?: boolean }>('h2')`
     font-size: 36px;
     line-height: 48px;
 
-    @media (max-width: 600px) {
+    @media (max-width: 800px) {
       display: block;
     }
   }
@@ -88,10 +104,49 @@ const LinkWrapper = styled.div`
   }
 `;
 
+interface AccessoryBannerData {
+  desktop: {
+    childImageSharp: {
+      fixed: FixedObject;
+    };
+  };
+  tablet: {
+    childImageSharp: {
+      fixed: FixedObject;
+    };
+  };
+}
+
 export default function AccessoryBanner() {
+  const data = useStaticQuery<AccessoryBannerData>(graphql`
+    fragment AccessoryBannerBg on File {
+      childImageSharp {
+        fixed(width: 1200, quality: 80) {
+          ...GatsbyImageSharpFixed_withWebp_noBase64
+        }
+      }
+    }
+
+    {
+      desktop: file(relativePath: {eq: "images/accessories/banner/1200.png"}) {
+        ...AccessoryBannerBg
+      }
+      tablet: file(relativePath: {eq: "images/accessories/banner/800.png"}) {
+        ...AccessoryBannerBg
+      }
+    }
+  `);
+
   return (
     <Container>
       <Background>
+        <picture>
+          <source srcSet={data.tablet.childImageSharp.fixed.srcSetWebp} type="image/webp" media="(max-width: 800px)" />
+          <source srcSet={data.tablet.childImageSharp.fixed.srcSet} type="image/png" media="(max-width: 800px)" />
+          <source srcSet={data.desktop.childImageSharp.fixed.srcSetWebp} type="image/webp" />
+          <source srcSet={data.desktop.childImageSharp.fixed.srcSet} type="image/png" />
+          <img src={data.tablet.childImageSharp.fixed.src} />
+        </picture>
       </Background>
       <Content>
         <ContentTitle>안심하고 책에만 집중하세요</ContentTitle>
