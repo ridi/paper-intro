799a71da7814dafd1f393f883ccc1fd06e14065e
준희(엔지니어) <junhee.han@ridi.com>
2022-03-20 23:28:42 +0900

    fix: 띄여쓰기 수정

--- src/components/ridipaper4/Design/Design.tsx
+++ src/components/ridipaper4/Design/Design.tsx
@@ -1,4 +1,4 @@
-import styled from 'astroturf'
+import styled from 'astroturf';
 import { graphql, useStaticQuery } from 'gatsby';
 import { useFloatText } from '@/components/ridipaper4/hooks/useFloatText';
 import { useRef } from 'react';
@@ -9,15 +9,19 @@ import { LineBreakOnMobile } from '@/components/ridipaper4/LineBreak';
 
 const buttonImagesQuery = graphql`
   query ButtonImagesQuery {
-    buttonDetails: file(relativePath: {eq: "images/ridipaper4/design/button-details.png"}) {
+    buttonDetails: file(
+      relativePath: { eq: "images/ridipaper4/design/button-details.png" }
+    ) {
       childImageSharp {
         fluid(maxWidth: 1000, quality: 90) {
           ...GatsbyImageSharpFluid_withWebp_noBase64
         }
       }
     }
-    
-    buttonLifestyleCut: file(relativePath: {eq: "images/ridipaper4/design/button-lifestylecut.png"}) {
+
+    buttonLifestyleCut: file(
+      relativePath: { eq: "images/ridipaper4/design/button-lifestylecut.png" }
+    ) {
       childImageSharp {
         fluid(maxWidth: 750, quality: 90) {
           ...GatsbyImageSharpFluid_withWebp_noBase64
@@ -49,7 +53,7 @@ const PrimaryTitle = styled('h2')`
   font-weight: 700;
   line-height: 48px;
   text-align: center;
-  
+
   @media (max-width: 600px) {
     font-size: 24px;
     line-height: 36px;
@@ -63,12 +67,12 @@ const PrimaryDescription = styled('p')`
   font-size: 18px;
   line-height: 30px;
   text-align: center;
-  
+
   @media (max-width: 600px) {
     font-size: 15px;
     line-height: 22px;
   }
-  
+
   @media (max-width: 400px) {
     font-size: 12px;
     padding: 0 10px;
@@ -90,7 +94,7 @@ const SecondaryTitle = styled('h3')`
   font-size: 28px;
   line-height: 37px;
   text-align: center;
-  
+
   @media (max-width: 600px) {
     font-size: 20px;
     line-height: 32px;
@@ -103,8 +107,8 @@ const SecondaryDescription = styled('p')`
   font-size: 14px;
   line-height: 19px;
   text-align: center;
-  opacity: .6;
-  
+  opacity: 0.6;
+
   @media (max-width: 400px) {
     font-size: 12px;
     line-height: 18px;
@@ -126,7 +130,7 @@ const ButtonLifestyleCutImageContainer = styled('div')`
   margin-top: 67px;
   border-radius: 26px;
   overflow: hidden;
-  
+
   @media (max-width: 600px) {
     border-radius: 0;
     width: 90%;
@@ -138,48 +142,52 @@ const ButtonLifestyleCutImageContainer = styled('div')`
 const PRIMARY_TEXT_FLOAT_OPTIONS = {
   duration: '30%',
   hook: 0.7,
-  height: 20
+  height: 20,
 };
 
 const SECONDARY_TEXT_FLOAT_OPTIONS = {
   duration: '30%',
   hook: 0.7,
-  height: 20
+  height: 20,
 };
 
 export const Design = (): JSX.Element => {
   const primaryTriggerRef = useRef<HTMLDivElement>(null);
   const secondaryTriggerRef = useRef<HTMLDivElement>(null);
-  const primaryFloatRef = useFloatText<HTMLDivElement>(primaryTriggerRef, PRIMARY_TEXT_FLOAT_OPTIONS);
-  const secondaryFloatRef = useFloatText<HTMLDivElement>(secondaryTriggerRef, SECONDARY_TEXT_FLOAT_OPTIONS);
-  
+  const primaryFloatRef = useFloatText<HTMLDivElement>(
+    primaryTriggerRef,
+    PRIMARY_TEXT_FLOAT_OPTIONS,
+  );
+  const secondaryFloatRef = useFloatText<HTMLDivElement>(
+    secondaryTriggerRef,
+    SECONDARY_TEXT_FLOAT_OPTIONS,
+  );
+
   const { buttonDetails, buttonLifestyleCut } = useStaticQuery<{
-    buttonDetails: { childImageSharp: { fluid: FluidObject } }
-    buttonLifestyleCut: { childImageSharp: { fluid: FluidObject } }
+    buttonDetails: { childImageSharp: { fluid: FluidObject } };
+    buttonLifestyleCut: { childImageSharp: { fluid: FluidObject } };
   }>(buttonImagesQuery);
-  
+
   return (
     <DesignContainer>
       <Trigger ref={primaryTriggerRef} />
       <TextContainer ref={primaryFloatRef}>
         <PrimaryTitle>감각적인 간결함을 드러내다</PrimaryTitle>
         <PrimaryDescription>
-          시간과 공간을 간결하게 채우는 당신,{' '}
-          <LineBreakOnMobile />
+          시간과 공간을 간결하게 채우는 당신, <LineBreakOnMobile />
           그것이 일상을 만드는 가장 당신다운 삶의 방식이니까
           <br />
-          미니멀 디자인의 RIDIPAPER 4는 시간, 공간,{' '}
-          <LineBreakOnMobile />
+          미니멀 디자인의 RIDIPAPER 4는 시간, 공간, <LineBreakOnMobile />
           그리고 당신의 감각까지 그대로 반영합니다.
         </PrimaryDescription>
       </TextContainer>
-      
+
       <DecoratorLine />
-      
+
       <ButtonDetailsImageContainer>
         <Img fluid={buttonDetails.childImageSharp.fluid} />
       </ButtonDetailsImageContainer>
-      
+
       <Trigger ref={secondaryTriggerRef} />
       <TextContainer ref={secondaryFloatRef}>
         <SecondaryTitle>
@@ -188,19 +196,16 @@ export const Design = (): JSX.Element => {
           새롭게 조형된 버튼
         </SecondaryTitle>
         <SecondaryDescription>
-          첫 페이지부터 마지막 페이지까지{' '}
-          <LineBreakOnMobile />
-          기분좋게 눌리도록 설계된 9.5mm의
-          <br />
-          원형 버튼. 몇 시간을 들고 있어도{' '}
-          <LineBreakOnMobile />
+          첫 페이지부터 마지막 페이지까지 <LineBreakOnMobile />
+          기분좋게 눌리도록 설계된 9.5mm의 원형 버튼.
+          <br />몇 시간을 들고 있어도 <LineBreakOnMobile />
           편하게 쥘 수 있도록 남겨놓은 여백.
         </SecondaryDescription>
       </TextContainer>
-      
+
       <ButtonLifestyleCutImageContainer>
         <Img fluid={buttonLifestyleCut.childImageSharp.fluid} />
       </ButtonLifestyleCutImageContainer>
     </DesignContainer>
   );
-}
+};
